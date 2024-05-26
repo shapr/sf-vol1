20c2157509a4a4067ee47ebe13ab9399953aafe0
Shae Erisson <shae@scannedinavian.com>
2024-05-26 17:08:07 -0400

    flake and basics.v

---
+++ 1basics.v
@@ -0,0 +1,19 @@
+Inductive day : Type :=
+  | monday
+  | tuesday
+  | wednesday
+  | thursday
+  | friday
+  | saturday
+  | sunday.
+
+Definition next_weekday (d:day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
